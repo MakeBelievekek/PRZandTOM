b95fa2060b8ecf2e05f60d39f837fac6d6e1ac30
TOM <matetothuk@gamil.com>
2020-04-02 01:01:39 +0200

    reaktive and some more

--- src/app/components/insurance/additional-insurance/additional-insurance.component.ts
+++ src/app/components/insurance/additional-insurance/additional-insurance.component.ts
@@ -1,24 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Page } from '../../../enum/page';
-import { UtilRepoService } from '../../../services/util-repo.service';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Page} from '../../../enum/page';
+import {UtilRepoService} from '../../../services/util-repo.service';
 
 @Component({
-    selector: 'app-additional-insurance',
-    templateUrl: './additional-insurance.component.html',
-    styleUrls: ['./additional-insurance.component.css'],
+  selector: 'app-additional-insurance',
+  templateUrl: './additional-insurance.component.html',
+  styleUrls: ['./additional-insurance.component.css'],
 })
 export class AdditionalInsuranceComponent implements OnInit {
 
-    additionalInsuranceForm: FormGroup;
+  show: boolean;
 
-    constructor(private utilRepoService: UtilRepoService, private formBuilder: FormBuilder) {
-        this.additionalInsuranceForm = this.formBuilder.group({});
-    }
+  additionalInsuranceForm: FormGroup;
 
-    ngOnInit(): void {
-        this.utilRepoService.progress.next(Page.SECOND);
-    }
+  constructor(private utilRepoService: UtilRepoService, private formBuilder: FormBuilder) {
+    this.additionalInsuranceForm = this.formBuilder.group({});
+  }
 
+  ngOnInit(): void {
+    this.utilRepoService.progress.next(Page.SECOND);
+  }
+
+  dummy() {
+    this.show = !this.show;
+  }
 
 }
