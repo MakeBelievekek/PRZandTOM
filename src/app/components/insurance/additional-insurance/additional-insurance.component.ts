578f3112c66cee9e4d11feeda6ff8481bf622b54
zsolt.preseka@gmail.com <Gyuszko1004>
2020-04-01 16:53:22 +0200

    router

--- src/app/components/insurance/additional-insurance/additional-insurance.component.ts
+++ src/app/components/insurance/additional-insurance/additional-insurance.component.ts
@@ -1,24 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Page } from '../../../enum/page';
-import { UtilRepoService } from '../../../services/util-repo.service';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Page} from '../../../enum/page';
+import {UtilRepoService} from '../../../services/util-repo.service';
+
+import * as $ from 'jquery';
+
+
 
 @Component({
-    selector: 'app-additional-insurance',
-    templateUrl: './additional-insurance.component.html',
-    styleUrls: ['./additional-insurance.component.css'],
+  selector: 'app-additional-insurance',
+  templateUrl: './additional-insurance.component.html',
+  styleUrls: ['./additional-insurance.component.css'],
 })
 export class AdditionalInsuranceComponent implements OnInit {
 
-    additionalInsuranceForm: FormGroup;
+  price : number = 500;
+  additionalInsuranceForm: FormGroup;
 
-    constructor(private utilRepoService: UtilRepoService, private formBuilder: FormBuilder) {
-        this.additionalInsuranceForm = this.formBuilder.group({});
-    }
+  constructor(private utilRepoService: UtilRepoService, private formBuilder: FormBuilder) {
+    this.additionalInsuranceForm = this.formBuilder.group({});
+  }
 
-    ngOnInit(): void {
-        this.utilRepoService.progress.next(Page.SECOND);
-    }
+  ngOnInit(): void {
+    this.utilRepoService.progress.next(Page.SECOND);
+    $('.counter-count').each(function () {
+      $(this).prop('Counter', 0).animate({
+        Counter: $(this).text()
+      }, {
+        duration: 5000,
+        easing: 'swing',
+        step: function (now) {
+          $(this).text(Math.ceil(now));
+        }
+      });
+    });
+  }
 
 
 }
